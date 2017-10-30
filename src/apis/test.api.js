e50aa2311cb69d0b6b4557a687c5470e9f0ee3a9
Appian <zhang4baichi@sina.com>
2017-10-30 17:24:34 +0800

    test ajax post

--- src/apis/test.api.js
+++ src/apis/test.api.js
@@ -2,7 +2,7 @@ import { ajaxGet, ajaxPost, query as urlQuery } from '../js/ajaxtools';
 
 // 获得初始化的列表
 export function getInitList(data) {
-	return ajaxGet('/auth', JSON.stringify(data));
+	return ajaxPost('/auth', JSON.stringify(data));
 }
 
 export const query = urlQuery;
