c360541670ebd927eda7e7f58c3927b37d1468f4
Appian <zhang4baichi@sina.com>
2017-12-08 18:39:07 +0800

    websocket 404 modify connect ip:port 8

--- src/js/test/index.js
+++ src/js/test/index.js
@@ -10,7 +10,7 @@ import io from 'socket.io-client';
     },
     mounted: function () {
       var isDev = window.location.origin.indexOf('localhost') > -1 || window.location.origin.indexOf('192') > -1;
-      var ip = isDev ? 'http://localhost:1600': 'http://localhost:3001'
+      var ip = isDev ? 'http://localhost:1600': window.location.origin
       this.socketClient = io.connect(ip);
 
       var that = this;
