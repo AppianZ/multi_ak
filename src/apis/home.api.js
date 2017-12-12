16f346c0cac9e4b1322970438f7c996ebe8c2ac8
Appian <zhang4baichi@sina.com>
2017-12-12 12:08:41 +0800

    修改了 /home/home.html的请求地址2

--- src/apis/home.api.js
+++ src/apis/home.api.js
@@ -2,7 +2,7 @@ import { ajaxGet, ajaxPost, query as urlQuery } from '../js/ajaxtools';
 
 // 获得初始化的列表
 export function getInitList(data) {
-	return ajaxPost('/auth', data);
+	return ajaxPost('/api/auth', data);
 }
 
 export function getTestSocket(data) {
