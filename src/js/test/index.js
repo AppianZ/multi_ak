c72f11f2be2e119558f1235301d94b02c1ab2637
Appian <zhang4baichi@sina.com>
2017-12-12 09:13:02 +0800

    修改了io.connect('ip')2

--- src/js/test/index.js
+++ src/js/test/index.js
@@ -10,7 +10,7 @@ import io from 'socket.io-client';
     },
     mounted: function () {
       var isDev = window.location.origin.indexOf('localhost') > -1 || window.location.origin.indexOf('192') > -1;
-      var ip = isDev ? 'http://localhost:1600': 'http://39.108.218.145:3100'
+      var ip = isDev ? 'http://localhost:1600': 'http://39.108.218.145'
       this.socketClient = io.connect(ip);
       console.log('ip1 --- ' + ip);
       var that = this;
